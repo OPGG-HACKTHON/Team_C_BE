8877296ed4c0402957640dae63e47e559abe4729
HYOTAEK <hyotaek9812@gmail.com>
2021-08-12 00:57:47 +0900

    feat: update getCurrentGame with include lastFinishedGame

--- controller/info/getCurrentGame.js
+++ controller/info/getCurrentGame.js
@@ -3,6 +3,17 @@ const gameRepo = require("../../dataAccess/game");
 const scheduleService = require("../../service/info/schedule");
 
 const getCurrentGame = async (req, res) => {
+	const lastFinishedGame = await gameRepo.getLastFinishedSchedule();
+
+	if (
+		new Date() <
+		new Date(
+			Date.parse(lastFinishedGame.dataValues.finishedAt) + 1000 * 60 * 30
+		)
+	) {
+		return res.json(resUtil.success(201, lastFinishedGame));
+	}
+
 	const currentGame = await gameRepo.getCurrentSchedule();
 	if (currentGame == null) {
 		return res.json(resUtil.success(201, currentGame));
@@ -10,6 +21,7 @@ const getCurrentGame = async (req, res) => {
 	const parsedCurGame = await scheduleService.parseCurGame(
 		currentGame.dataValues
 	);
+
 	res.json(resUtil.success(201, parsedCurGame));
 };
 
