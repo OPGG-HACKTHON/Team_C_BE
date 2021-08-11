6b462094b2e2a1845321a77d2fb0c435f8f7dfa2
jungpyolee <80612689+jungpyolee@users.noreply.github.com>
2021-08-11 16:05:15 +0900

    create appleauth file

--- router/auth.js
+++ router/auth.js
@@ -3,32 +3,52 @@ const router = express.Router();
 const passport = require("passport");
 const wrapAsync = require("../util/wrapAsync");
 const kakaoService = require("../service/oAuthAPI/kakao");
+const appleService = require("../service/oAuthAPI/apple");
 
 const kakaoAuthenticate = require("../controller/oAuthApi/kakaoAuthenticate");
+const appleAuthenticate = require("../controller/oAuthApi/appleAuthenticate");
 const getUser = require("../controller/oAuthApi/getUser");
 kakaoService.kakaoSerializer();
 kakaoService.kakaoDeserializer();
 kakaoService.kakaoUse();
 
+appleService.appleSerializer();
+appleService.appleDeserializer();
+appleService.appleUse();
+
 //localhost:5000/auth/kakao로 들어오면 (get으로 들어오면) passport.authenticate를 실행( 임의로 login-kakao로 명명)
 router.get(
-	"/kakao",
-	passport.authenticate("login-kakao"),
-	wrapAsync(kakaoAuthenticate)
+  "/kakao",
+  passport.authenticate("login-kakao"),
+  wrapAsync(kakaoAuthenticate)
 );
 
 router.get(
-	"/kakao/callback",
-	passport.authenticate("login-kakao", {
-		successRedirect: "http://localhost:3000/success",
-		failureRedirect: "/auth/kakao/fail",
-	})
+  "/kakao/callback",
+  passport.authenticate("login-kakao", {
+    successRedirect: "http://localhost:3000/success",
+    failureRedirect: "/auth/kakao/fail",
+  })
 );
 
 router.get(
-	"/getUser",
+  "/apple",
+  passport.authenticate("login-apple"),
+  wrapAsync(appleAuthenticate)
+);
 
-	wrapAsync(getUser)
+router.get(
+  "/apple/callback",
+  passport.authenticate("login-apple", {
+    successRedirect: "http://localhost:3000/success",
+    failureRedirect: "/auth/apple/fail",
+  })
+);
+
+router.get(
+  "/getUser",
+
+  wrapAsync(getUser)
 );
 
 module.exports = router;
