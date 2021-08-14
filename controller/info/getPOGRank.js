7bf72be71870e3abd7892debaf2eeac4b31ffc59
HYOTAEK <hyotaek9812@gmail.com>
2021-08-14 14:19:04 +0900

    feat: getPOGRank에 rank 필드 추가

--- controller/info/getPOGRank.js
+++ controller/info/getPOGRank.js
@@ -4,6 +4,12 @@ const playerRepo = require("../../dataAccess/player");
 const getPOGRank = async (req, res) => {
 	const pogRank = await playerRepo.getPOGRank();
 
+	let rank = 1;
+	for (const player of pogRank) {
+		player.dataValues.rank = rank;
+		rank += 1;
+	}
+
 	res.json(resUtil.success(201, pogRank));
 };
 
