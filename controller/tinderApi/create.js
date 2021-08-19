6ca0b4ff68e394b24fc569498c7621b7a97f2e51
jungpyolee <80612689+jungpyolee@users.noreply.github.com>
2021-08-19 10:54:18 +0900

    중복제거

--- controller/tinderApi/create.js
+++ controller/tinderApi/create.js
@@ -9,37 +9,23 @@ const create = async (req, res) => {
   const userId = req.userId;
   const teamId = await getTeamIdbyUserId(userId);
   const reportedCount = await getReportedCountbyUserId(userId);
+  let body = { userId: userId, msg: req.body.msg, teamId: teamId };
   if (reportedCount < 5) {
     if (req.body.gameId) {
-      const body = {
+      body = {
         userId: userId,
         msg: req.body.msg,
+        teamId: teamId,
         gameId: req.body.gameId,
-        teamId: teamId,
       };
+    }
 
-      const result = await createTinder(body);
+    const result = await createTinder(body);
 
-      if (result === "success") {
-        res.json(resUtil.success(200, "틴더 생성 성공"));
-      } else {
-        res.json(resUtil.fail(400, "틴더 생성 실패"));
-      }
+    if (result === "success") {
+      res.json(resUtil.success(200, "틴더 생성 성공"));
     } else {
-      const body = {
-        userId: userId,
-        msg: req.body.msg,
-        teamId: teamId,
-        gameId: null,
-      };
-
-      const result = await createTinder(body);
-
-      if (result === "success") {
-        res.json(resUtil.success(200, "틴더 생성 성공"));
-      } else {
-        res.json(resUtil.fail(400, "틴더 생성 실패"));
-      }
+      res.json(resUtil.fail(400, "틴더 생성 실패"));
     }
   } else {
     res.json(resUtil.fail(401, "틴더 생성이 금지된 유저입니다."));
