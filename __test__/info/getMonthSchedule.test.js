a9ba3a5ce427ba38f864a01d8e9b8a50559359b4
HYOTAEK <hyotaek9812@gmail.com>
2021-08-20 14:28:30 +0900

    hofix: fix test error

--- __test__/info/getMonthSchedule.test.js
+++ __test__/info/getMonthSchedule.test.js
@@ -4,16 +4,16 @@ const request = require("supertest");
 jest.setTimeout(10000);
 
 afterAll(async () => {
-  server.close();
+	server.close();
 });
 
 describe("GET /info/schedule?month=8", () => {
-  test("we got data.status 201", async () => {
-    const res = await request(server)
-      .get("/info/schedule")
-      .query({ month: 8 })
-      .set("Accept", "application/json");
+	test("we got data.status 200", async () => {
+		const res = await request(server)
+			.get("/info/schedule")
+			.query({ month: 8 })
+			.set("Accept", "application/json");
 
-    expect(res.body.status).toBe(201);
-  });
+		expect(res.body.status).toBe(200);
+	});
 });
