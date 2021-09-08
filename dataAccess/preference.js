adee91335e426e6c1048cb7127bc08143a3dde03
HYOTAEK <hyotaek9812@gmail.com>
2021-09-08 19:49:05 +0900

    feat: getUserPreference

--- dataAccess/preference.js
+++ dataAccess/preference.js
@@ -1,4 +1,5 @@
 const { Preference } = require("../models/index");
+const { Player } = require("../models/index");
 const { Op } = require("sequelize");
 
 module.exports = {
@@ -17,6 +18,16 @@ module.exports = {
 			const result = await Preference.create({
 				userId: userId,
 				playerId: playerId,
+			});
+			res(result);
+		});
+	},
+	getUserPreference: (userId) => {
+		return new Promise(async (res, rej) => {
+			const result = await Preference.findAll({
+				include: [{ model: Player }],
+				attributes: ["userId"],
+				where: { userId: userId },
 			});
 			res(result);
 		});
--- router/userSetting.js
+++ router/userSetting.js
@@ -5,11 +5,13 @@ const { checkTokens } = require("../util/checkTokens");
 const updateNickname = require("../controller/userSettingApi/updateNickname");
 const updateTeamId = require("../controller/userSettingApi/updateTeamId");
 const savePreference = require("../controller/userSettingApi/savePreference");
+const getUserPreference = require("../controller/userSettingApi/getUserPreference");
 
 const router = express.Router();
 
 router.put("/nickname", wrapAsync(checkTokens), wrapAsync(updateNickname));
 router.put("/teamId", wrapAsync(checkTokens), wrapAsync(updateTeamId));
 router.post("/preference", wrapAsync(checkTokens), wrapAsync(savePreference));
+router.get("/preference", wrapAsync(checkTokens), wrapAsync(getUserPreference));
 
 module.exports = router;
